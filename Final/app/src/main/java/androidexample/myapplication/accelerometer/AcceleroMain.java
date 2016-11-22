a6f4c91f2bf153160fbdbe8e3b7eedc00d84a169
sdi1300204 <sdi1300204@di.uoa.gr>
2016-11-22 15:32:06 +0200

    sxediastika

--- Final/app/src/main/java/androidexample/myapplication/accelerometer/AcceleroMain.java
+++ Final/app/src/main/java/androidexample/myapplication/accelerometer/AcceleroMain.java
@@ -34,7 +34,7 @@ public class AcceleroMain extends seekbar implements SensorEventListener{
 		this.tvX=tvX;
 		this.tvY=tvY;
 		this.tvZ=tvZ;
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		//mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
 	}
